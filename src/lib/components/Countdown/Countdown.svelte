3d692b5502cff723b063345469b01389467316f2
Dave Beazer <hi@davebeazer.dev>
2022-12-27 00:04:07 +0100

    feat: rough cut background image sequence

---
+++ src/lib/components/Countdown/Countdown.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	/**
+	 * @type {any[]}
+	 */
+	let bgImage: string = '';
+
+	const bgClasses = [
+		'bg-golf-1',
+		'bg-golf-2',
+		'bg-golf-3',
+		'bg-golf-4',
+		'bg-golf-5',
+		'bg-golf-6',
+		'bg-golf-7',
+		'bg-golf-8',
+		'bg-golf-9'
+	];
+	function randomIx() {
+		return Math.floor(Math.random() * bgClasses.length);
+	}
+
+	onMount(async () => {
+		setInterval(() => {
+			bgImage = bgClasses[randomIx()];
+		}, 5000);
+	});
+</script>
+
+
+
+<div class="flex w-screen h-screen bg-no-repeat bg-cover {bgImage}" />
