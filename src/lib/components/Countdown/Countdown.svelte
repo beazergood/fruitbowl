728dc2a71e06e1302cc2fe381943d6133d39bcf8
Dave Beazer <hi@davebeazer.dev>
2022-12-31 19:16:15 +0000

    feat: rough cut of home page widgets

--- src/lib/components/Countdown/Countdown.svelte
+++ src/lib/components/Countdown/Countdown.svelte
@@ -22,12 +22,43 @@
 	}
 
 	onMount(async () => {
-		setInterval(() => {
-			bgImage = bgClasses[randomIx()];
-		}, 5000);
+		bgImage = bgClasses[randomIx()];
 	});
 </script>
 
+<div
+	class="flex flex-col gap-4 justify-start w-screen h-screen bg-no-repeat bg-contain p-4 pt-10 bg-green-700 {bgImage}"
+>
+	<!-- Info widget -->
+	<div class="bg-white/30 mt-10 flex flex-col p-4 rounded-lg">
+		<h1 class="text-3xl font-bold text-center text-gray-700">Fruitbowl 2023</h1>
+		<h3 class="text-xl font-bold text-center text-gray-700">🫐 🍉 🍊 🥑</h3>
+		<h3 class="text-lg font-bold text-center text-gray-700">Palm Desert, CA 🇺🇸</h3>
+	</div>
 
+	<!-- Date Countdown widget -->
+	<div class="bg-white/80 flex flex-col p-4 rounded-lg border-2 border-white">
+		<h1 class="text-3xl font-bold text-center text-gray-700"><span class="text-lg mr-2">In</span>48 Days</h1>
+		<h3 class="text-3xl font-bold text-center">17 - 20 Feb, 2023</h3>
+	</div>
 
-<div class="flex w-screen h-screen bg-no-repeat bg-cover {bgImage}" />
+	<!-- Map widget TODO: add mapbox map -->
+	<div class="bg-white/80 flex flex-col p-4 rounded-lg border-2 border-white">
+		<h1 class="text-md font-bold text-left text-gray-700">Where</h1>
+	</div>
+
+	<!-- Transport widget -->
+	<div class="bg-white/80 flex flex-col p-4 rounded-lg border-2 border-white">
+		<h1 class="text-md font-bold text-left text-gray-700">Transport</h1>
+	</div>
+
+	<!-- Accomodation widget -->
+	<div class="bg-white/80 flex flex-col p-4 rounded-lg border-2 border-white">
+		<h1 class="text-md font-bold text-left text-gray-700">Accomodation</h1>
+	</div>
+
+	<!-- Weather widget -->
+	<div class="bg-white/80 flex flex-col p-4 rounded-lg border-2 border-white">
+		<h1 class="text-md font-bold text-left text-gray-700">Weather</h1>
+	</div>
+</div>
