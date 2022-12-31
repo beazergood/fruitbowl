78a89658575837e54445e408f0efa468b906f07a
Dave Beazer <hi@davebeazer.dev>
2022-12-31 21:08:14 +0000

    feat: wire up countdown to start date

--- src/lib/components/Countdown/Countdown.svelte
+++ src/lib/components/Countdown/Countdown.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { DateTime } from 'luxon';
 	import { onMount } from 'svelte';
 
 	/**
@@ -17,12 +18,21 @@
 		'bg-golf-8',
 		'bg-golf-9'
 	];
+
 	function randomIx() {
 		return Math.floor(Math.random() * bgClasses.length);
 	}
 
+	let diffInDays:number;
 	onMount(async () => {
 		bgImage = bgClasses[randomIx()];
+
+		var end = DateTime.fromISO('2023-02-17');
+		var start = DateTime.now();
+
+		diffInDays = Math.floor(end.diff(start, 'days').toObject().days);
+		console.log('diffInMonths',diffInDays);
+		// diffInDays.toObject(); //=> { months: 1 }
 	});
 </script>
 
@@ -30,7 +40,7 @@
 	class="flex flex-col gap-4 justify-start w-screen h-screen bg-no-repeat bg-contain p-4 pt-10 bg-green-700 {bgImage}"
 >
 	<!-- Info widget -->
-	<div class="bg-white/30 mt-10 flex flex-col p-4 rounded-lg">
+	<div class="bg-white/50 mt-10 flex flex-col p-4 rounded-lg">
 		<h1 class="text-3xl font-bold text-center text-gray-700">Fruitbowl 2023</h1>
 		<h3 class="text-xl font-bold text-center text-gray-700">🫐 🍉 🍊 🥑</h3>
 		<h3 class="text-lg font-bold text-center text-gray-700">Palm Desert, CA 🇺🇸</h3>
@@ -38,8 +48,10 @@
 
 	<!-- Date Countdown widget -->
 	<div class="bg-white/80 flex flex-col p-4 rounded-lg border-2 border-white">
-		<h1 class="text-3xl font-bold text-center text-gray-700"><span class="text-lg mr-2">In</span>48 Days</h1>
-		<h3 class="text-3xl font-bold text-center">17 - 20 Feb, 2023</h3>
+		<h1 class="text-3xl font-bold text-center text-gray-700">
+			<span class="text-lg mr-2">In</span>{diffInDays} Days
+		</h1>
+		<h3 class="text-3xl font-bold text-center text-gray-700">17 - 20 Feb, 2023</h3>
 	</div>
 
 	<!-- Map widget TODO: add mapbox map -->
