f73485e516ae7a2e0b0a60999c7c58dc6d1801c9
Dave Beazer <hi@davebeazer.dev>
2022-12-31 19:00:38 +0000

    refactor: page to only have Countdown component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,6 @@
 
 <div class="flex justify-center h-screen">
     <div class="flex flex-col justify-center">
-        <h1 class="text-3xl font-bold underline text-red-300">Fruitbowl 2023</h1>    
         <Countdown />
     </div>
 </div>
