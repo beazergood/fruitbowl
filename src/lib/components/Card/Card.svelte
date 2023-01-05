a83348924dfebe394cc94d1b93f1f2b2753b9670
Dave Beazer <hi@davebeazer.dev>
2023-01-05 17:56:27 +0000

    refactor: card background

--- src/lib/components/Card/Card.svelte
+++ src/lib/components/Card/Card.svelte
@@ -3,7 +3,7 @@
 	export let metadata: Metadata;
 </script>
 
-<div class="bg-white/100 flex flex-col p-2 rounded-lg border-2 {metadata.borderClass}">
+<div class="bg-white/80 flex flex-col p-2 rounded-lg border-2 {metadata.borderClass}">
 	<slot name="title" class="text-md text-left lext-lg text-gray-700 font-semibold mb-2"/>
 	<slot name="content" />
 </div>
