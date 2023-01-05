3d5923a71bf07e00dbb06ffaaa7f7695183f1756
Dave Beazer <hi@davebeazer.dev>
2023-01-05 17:55:32 +0000

    refactor: event pages and data

--- src/routes/saalbach/+page.svelte
+++ src/routes/saalbach/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-	import Countdown from '../../lib/components/Countdown/Countdown.svelte';
-    import data from './viewModel'
+	import Dashboard from '../../lib/components/Dashboard/Dashboard.svelte';
+	import data from './viewModel';
 </script>
 
 <div class="flex justify-center h-screen">
-    <div class="flex flex-col ">
-        <Countdown event={data.event}/>
-    </div>
+	<div class="flex flex-col ">
+        <Dashboard data={data} />
+	</div>
 </div>
