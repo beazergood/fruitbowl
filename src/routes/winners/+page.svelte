706e069d40da9ee1c8b4c1048822580db3e4036c
Dave Beazer <hi@davebeazer.dev>
2023-01-05 11:01:06 +0000

    feat: add winners page and HonorsBoard component

---
+++ src/routes/winners/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import HonorsBoard from '../../lib/components/HonorsBoard/HonorsBoard.svelte';
+    import data from './viewModel'
+
+</script>
+
+<div class="flex justify-start w-full h-screen">
+    <div class="flex flex-col ">
+        <HonorsBoard {data} />
+    </div>
+</div>
