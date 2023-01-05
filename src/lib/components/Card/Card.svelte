d0ff3de0fe5793d9d5f66b41bf2fccd5a35f0858
Dave Beazer <hi@davebeazer.dev>
2023-01-05 11:30:19 +0000

    chore: removed cache

--- src/lib/components/Card/Card.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import type { Metadata } from './$types';
-	export let metadata: Metadata;
-</script>
-
-<div class="bg-white/100 flex flex-col p-2 rounded-lg border-2 {metadata.borderClass}">
-	<slot name="title" class="text-md text-left lext-lg text-gray-700 font-semibold mb-2"/>
-	<slot name="content" />
-</div>
--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Countdown from '../lib/components/Countdown/Countdown.svelte';
-</script>
-
-<div class="flex justify-center h-screen">
-    <div class="flex flex-col ">
-        <Countdown />
-    </div>
-</div>
