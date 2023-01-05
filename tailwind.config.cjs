12d5c6ab1a2e7919f0d7587f46fad160c57bc64c
Dave Beazer <hi@davebeazer.dev>
2023-01-05 17:55:47 +0000

    fix: tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,20 +3,18 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],  theme: {
     extend: {
       backgroundImage: {
-        'golf-1': "url('/images/IMG_4469.jpeg')",
-        'golf-2': "url('/images/IMG_4470.jpeg')",
-        'golf-3': "url('/images/IMG_4476.jpeg')",
-        'golf-4': "url('/images/IMG_4477.jpeg')",
-        'golf-5': "url('/images/IMG_4478.jpeg')",
-        'golf-6': "url('/images/IMG_4479.jpeg')",
-        'golf-7': "url('/images/IMG_4493.jpeg')",
-        'golf-8': "url('/images/IMG_4496.jpeg')",
-        'golf-9': "url('/images/IMG_4502.jpeg')",
+        'golf-1': "url('/images/IMG_4469.jpg')",
+        'golf-2': "url('/images/IMG_4470.jpg')",
+        'golf-3': "url('/images/IMG_4476.jpg')",
+        
+        
+        'golf-7': "url('/images/IMG_4493.jpg')",
+        
         'ski-1': "url('/images/saalbach_1.jpg')",
         'ski-2': "url('/images/saalbach_2.jpg')",
-        'avocado-1': "url('/images/avocado.jpeg')",
-        'citrus-1': "url('/images/citrus.jpeg')",
-        'marrionberry-1': "url('/images/marrionberry.jpeg')",
+        'avocado-1': "url('/images/avocado.jpg')",
+        'citrus-1': "url('/images/citrus.jpg')",
+        'marrionberry-1': "url('/images/marrionberry.jpg')",
         'frame': "url('/svg/frame.svg')",
         'frame-1': "url('/svg/frame-w.svg')",
       }
