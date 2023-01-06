fb15f077dfa40fd2b2d0349f8cafb6dc17ee3439
Dave Beazer <hi@davebeazer.dev>
2023-01-06 09:08:30 +0000

    feat: add CSS snow effect to ski page

--- src/routes/saalbach/+page.svelte
+++ src/routes/saalbach/+page.svelte
@@ -3,8 +3,12 @@
 	import data from './viewModel';
 </script>
 
+<head>
+	<script src="https://app.embed.im/snow.js" defer></script>
+</head>
+
 <div class="flex justify-center h-screen">
 	<div class="flex flex-col ">
-        <Dashboard data={data} />
+		<Dashboard {data} />
 	</div>
 </div>
