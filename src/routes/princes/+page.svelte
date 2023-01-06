86bf32164c955495d74e6c1971020b70a9489585
Dave Beazer <hi@davebeazer.dev>
2023-01-06 15:02:31 +0000

    feat: add Kent event

---
+++ src/routes/princes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Dashboard from '../../lib/components/Dashboard/Dashboard.svelte';
+	import data from './viewModel';
+</script>
+
+<div class="flex justify-center h-screen">
+	<div class="flex flex-col">
+        <Dashboard data={data} />
+	</div>
+</div>
---
+++ src/routes/princes/viewModel.ts
@@ -0,0 +1,81 @@
+const data = {
+	id: 2,
+	info: {
+		name: "Shane's 40th",
+		emoji: '🏌🏻 🏴󠁧󠁢󠁥󠁮󠁧󠁿 🍀 🍺',
+		startDate: '2023-03-16',
+		endDate: '2023-03-18'
+	},
+	location: {
+		city: 'Sandwich',
+		state: 'Fuckin Kent',
+		country: 'UK',
+		lat: 51.29578638091663,
+		lng: 1.3708297050788336,
+		zoom: 12,
+		timezone: 'America%2FLos_Angeles',
+		temperatureGuage: 'C'
+	},
+	metadata: {
+		headerColorClass: 'text-green-900',
+		bgColorClass: 'bg-green-800',
+		borderClass: 'border-green-300',
+		bgImageClasses: [
+			'bg-princes-1',
+			'bg-princes-2'
+		]
+	},
+	geoWaypoints: {
+		title: '📍 Where',
+		type: 'FeatureCollection',
+		features: [
+			{
+				type: 'Feature',
+				properties: {
+					description: 'Air BnB',
+					iconSize: [24, 24],
+					icon: '🏠'
+				},
+				geometry: {
+					type: 'Point',
+					coordinates: [1.3708297050788336, 51.29378638091663]
+				}
+			},
+			{
+				type: 'Feature',
+				properties: {
+					description: 'Air BnB',
+					iconSize: [24, 24],
+					icon: '🏌🏻'
+				},
+				geometry: {
+					type: 'Point',
+					coordinates: [1.3708297050788336, 51.29578638091663]
+				}
+			}
+		]
+	},
+	accommodation: {
+		title: '🏡 Accommodation',
+		name: 'Princes Golf Club Lodge',
+		description:
+			"Ask about the burger Collin Morikawa had after winning the Open, you'll get a free headache",
+		url: 'https://www.princesgolfclub.co.uk/',
+		location: {
+			lat: 51.29578638091663,
+			lng: 1.3708297050788336,
+			city: 'Sandwich'
+		}
+	},
+	itinerary: {
+		title: '🎯 Itinerary',
+		items: [
+			'3/16 - 🚙 to Kent, stay in Air BnB',
+			'3/17 - 18 🏌🏻 @ Princes',
+			'2/18 - 18 🏌🏻 @ Princes, 🚙 home',
+			"2/19 - Don't forget Mothers Day!"
+		]
+	}
+};
+
+export default data;
