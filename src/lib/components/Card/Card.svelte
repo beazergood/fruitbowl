51bdb810a7dde4498aabe9c6ea83cc047a25d83a
Dave Beazer <hi@davebeazer.dev>
2023-01-06 15:25:20 +0000

    refactor: card to have bg-color prop

--- src/lib/components/Card/Card.svelte
+++ src/lib/components/Card/Card.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import type { Metadata } from './$types';
 	export let metadata: Metadata;
+	export let bgColor = 'bg-white';
 </script>
 
-<div class="relative bg-white flex flex-col rounded-lg border-2 {metadata.borderClass}">
+<div class="relative {bgColor} flex flex-col rounded-lg border-2 {metadata.borderClass}">
 	<slot name="title"/>
 	<slot name="content" />
 </div>
