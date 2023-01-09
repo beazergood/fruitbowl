122580dee129b9681bc17fd88245c1f4f9cf1b76
Dave Beazer <dave@sideprojectco.com>
2023-01-09 09:19:44 +0000

    chore: switching machines

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -58,3 +58,27 @@ export const setTransportationForEvent = async (data) => {
 	console.log('updatedEvents: ', updatedEvents);
 	eventsStore.set(updatedEvents);
 };
+
+export function createLocalStorageStore(key: string, defaultValue: never[]) {
+    const store = writable(defaultValue);
+
+    if(typeof window !== 'undefined'){
+
+        const storedValue = JSON.parse(window.localStorage.getItem(key));
+        if (storedValue !== null) {
+            store.set(storedValue);
+        }
+        
+        store.subscribe(value => {
+            window.localStorage.setItem(key, JSON.stringify(value));
+        });
+    }
+
+    return store;
+}
+
+export function addToStore(newEntry) {
+    myStore.update(oldArray => [...oldArray, newEntry]);
+}
+
+export const myStore = createLocalStorageStore('events', []);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,16 @@
 	import { eventsStore } from '../lib/store';
 	import EventList from '../lib/components/EventList/EventList.svelte';
 	import type { Event } from './$types';
+	import Navbar from '../lib/components/Navbar/Navbar.svelte';
 
 	let events: Event[] = [];
 
 	eventsStore.subscribe((data) => {
-		console.log('events subscribe: ', data);
 		events = data;
 	});
 </script>
 
+<Navbar />
 <div class="flex justify-center h-screen">
 	<div class="flex flex-col ">
 		<h1>Homepage</h1>
