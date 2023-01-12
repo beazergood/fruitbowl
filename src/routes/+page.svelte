53adc98851a0f7349169172ded18a45a8926957b
Dave Beazer <hi@davebeazer.dev>
2023-01-12 17:18:23 +0000

    feat: add events list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,19 @@
-<script>
+<script lang="ts">
 	import { myStore } from '../lib/store';
-	import Navbar from '../lib/components/Navbar/Navbar.svelte';
-	
-	let events;
+	import EventList from '../lib/components/EventList/EventList.svelte';
+	import type { Event } from './$types';
+
+	let events: Event[] = [];
+
 	myStore.subscribe((data) => {
 		console.log('events subscribe: ', data);
 		events = data;
 	});
 </script>
 
-<Navbar />
 <div class="flex justify-center h-screen">
 	<div class="flex flex-col ">
 		<h1>Homepage</h1>
-		{events.length}
+		<EventList data={events} />
 	</div>
 </div>
