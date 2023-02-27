c7bf16542a2b38af3c2ac5a70df6b11aa1143908
Dave Beazer <hi@davebeazer.dev>
2023-02-27 12:22:22 +0000

    feat: cater for past events

--- src/lib/components/Countdown/Countdown.svelte
+++ src/lib/components/Countdown/Countdown.svelte
@@ -8,13 +8,14 @@
 
 	let diffInDays: number | string = '...';
 	let displayDates = '';
+	let futureEvent = true;
 
 	onMount(async () => {
 		var end = DateTime.fromISO(data.startDate);
 		var start = DateTime.now();
 		const diff = end.diff(start, 'days').toObject().days ?? 0;
 		diffInDays = Math.floor(diff);
-
+		futureEvent = diffInDays > 0;
 		const startDate = DateTime.fromISO(data.startDate).toFormat('dd MMM');
 		const endDate = DateTime.fromISO(data.endDate).toFormat('dd MMM yy');
 		displayDates = `${startDate} - ${endDate}`;
@@ -25,7 +26,13 @@
 	<div slot="title" class="text-md text-left lext-lg text-gray-700 font-semibold p-2">📅 When</div>
 	<div slot="content" class="px-2 pb-2">
 		<h1 class="text-3xl font-bold text-center text-gray-700 -mt-4">
-			<span class="text-lg mr-2">In</span>{diffInDays} Days
+			{#if futureEvent}
+				<span class="text-lg mr-2"> In </span>
+				{diffInDays} Days
+			{/if}
+			{#if !futureEvent}
+				{Math.abs(diffInDays)} days ago
+			{/if}
 		</h1>
 		<h3 class="text-xl font-bold text-center text-gray-700">{displayDates}</h3>
 	</div>
