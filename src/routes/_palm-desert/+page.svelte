ecca6d8540fb01de2cf2a8242b6c1393e9c98030
Dave Beazer <dave@sideprojectco.com>
2023-02-27 10:47:39 +0000

    fix: layout bug for winners button

--- src/routes/_palm-desert/+page.svelte
+++ src/routes/_palm-desert/+page.svelte
@@ -6,8 +6,8 @@
 <div class="flex justify-center h-screen">
 	<div class="flex flex-col">
 		<Dashboard {data} />
-		<div class="relative my-14 bg-green-600">
-			<div class="absolute top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+		<!-- <div class="relative my-14 bg-green-600">
+			<div class="absolute  left-1/2 transform -translate-x-1/2 -translate-y-1/2">
 				<a
 					class="text-center bottom-24 bg-white/80
                 my-2 border border-gray-300 rounded-md p-2"
@@ -15,6 +15,6 @@
 					>Past Winners 🏆
 				</a>
 			</div>
-		</div>
+		</div> -->
 	</div>
 </div>
