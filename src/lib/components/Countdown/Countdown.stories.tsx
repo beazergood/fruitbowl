c59de972fdae889741df87a4700e0e824ff070ee
Dave Beazer <hi@davebeazer.dev>
2023-02-27 12:22:22 +0000

    feat: cater for past events

--- src/lib/components/Countdown/Countdown.stories.tsx
+++ src/lib/components/Countdown/Countdown.stories.tsx
@@ -20,7 +20,16 @@ const Template = ({ ...args }) => ({
 	props: args
 });
 
-export const Default: Story = {
+export const FutureEvent: Story = {
+	args: {
+        data: {
+            meta: { borderClass: 'border-red-500', bgClasses: 'bg-gradient-to-b from-cyan-500 to-blue-500' },
+			startDate: '2024-01-13',
+			endDate: '2024-01-15',
+		}
+	}
+};
+export const PastEvent: Story = {
 	args: {
         data: {
             meta: { borderClass: 'border-red-500', bgClasses: 'bg-gradient-to-b from-cyan-500 to-blue-500' },
